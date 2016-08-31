ce4b7b2b152469a44e10cb1016c655d755f66279
Bright <nzsyl0203@gmail.com>
2016-08-31 21:48:24 +1200

    es6 initial
    let;
    const;

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// if (true) {
+// 	var fruit = 'apple';
+// 	let vege = 'mushroom';
+// }
+
+// console.log(fruit);console.log(vege);
+
+
+const color = ['red']
+console.log(color)
+color.push('green','blue')
+console.log(color)
